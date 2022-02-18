c18328b5622a653d878c1682019a5fad51c6ebde
armaBurton <armanarma@armaburton.com>
2022-02-18 15:10:02 -0800

    fixed the data transfer issue from the backend to the front

--- functions/hearthstone.js
+++ functions/hearthstone.js
@@ -5,24 +5,19 @@ require('dotenv').config();
 
 exports.handler = async (event) => {
   try {
-  //   const response = await fetch(`https://omgvamp-hearthstone-v1.p.rapidapi.com/cards?x-rapidapi-host=omgvamp-hearthstone-v1.p.rapidapi.com&x-rapidapi-key=f351f32ca5msh9d7f14c07a67b64p1ef1bdjsnebe9e76d4ae9`);
-  
-    // const response = await fetch(`https://omgvamp-hearthstone-v1.p.rapidapi.com/cards?x-rapidapi-host=omgvamp-hearthstone-v1.p.rapidapi.com&x-rapidapi-key=9182879981msh6217a65e8920b12p1e4338jsnfb0af2c5ad1b`);
-    // const json = await response.json();
 
-    const response = await fetch('https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/search/arth', {
-      'method': 'GET',
+    const response = await fetch('https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/search/volley', {
+      // 'method': 'GET',
       'headers': {
         'x-rapidapi-host': process.env.REACT_APP_X_RAPIDAPI_HOST,
         'x-rapidapi-key': process.env.REACT_APP_X_RAPIDAPI_KEY
       }
     });
-
-    console.log(`|| response >`, await response.json());
-
-    return { 
-      statusCode: 200, 
-      // body: JSON.stringify(json),
+    const data = await response.json();
+    console.log(`|| data >`, data);
+    return {
+      statusCode:200, 
+      body: JSON.stringify(data),
     };
   } catch (error) {
     console.log(error);
--- src/ListPage/ListPage.js
+++ src/ListPage/ListPage.js
@@ -2,21 +2,18 @@ import { useEffect, useState } from 'react';
 
 export default function ListPage({ user }){
   const [cards, setCards] = useState();
-  console.log(`|| user >`, user);
-  console.log(process.env.REACT_APP_X_RAPIDAPI_HOST);
-  console.log(process.env.REACT_APP_X_RAPIDAPI_KEY);
-
 
   useEffect(() => {
     async function fetchCards(){
       const response = await fetch(`/.netlify/functions/hearthstone/`);
 
-      const data = await response.json();
-      console.log(`|| data >`, data);
-      setCards(data);
+      // const data = await response.json();
+      console.log(`|| response >`, await response.json());
+      // console.log(`|| data >`, data);
+      // setCards(data);
     }
     fetchCards();
   }, []);
   
-  return <div>{cards}</div>;
+  return <div></div>;
 }
